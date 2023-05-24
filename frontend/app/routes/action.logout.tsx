04cdae55f9e70916b56b28c94b1d07400af14c4a
rajni-persaud <rajnipersaud246@gmail.com>
2023-05-24 04:05:37 -0400

    on logout, go to login page

--- frontend/app/routes/action.logout.tsx
+++ frontend/app/routes/action.logout.tsx
@@ -2,5 +2,5 @@ import { ActionArgs } from "@remix-run/node";
 import { authenticator } from "~/auth.server";
 
 export async function loader({request}: ActionArgs) {
-    return authenticator.logout(request, {redirectTo: "/"});
+    return authenticator.logout(request, {redirectTo: "/login"});
 }
