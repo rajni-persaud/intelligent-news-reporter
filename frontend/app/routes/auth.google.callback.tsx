f75e1b3eb87285eeb3e49d1a94f18f8e291f07a6
rajni-persaud <rajnipersaud246@gmail.com>
2023-05-24 04:16:49 -0400

    if login success, go to newsfeed

--- frontend/app/routes/auth.google.callback.tsx
+++ frontend/app/routes/auth.google.callback.tsx
@@ -3,7 +3,7 @@ import { authenticator } from "~/auth.server";
 
 export let loader = ({ request }: LoaderArgs) => {
   return authenticator.authenticate("google", request, {
-    successRedirect: "/dashboard",
+    successRedirect: "/newsfeed",
     failureRedirect: "/login",
   });
 };
